ff6bfb5c3ab0e2becda87096ad4e597cf042ae5a
Ajay Patil <aj41093@gmail.com>
2024-08-21 22:45:11 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// JavaScript for dynamic text transition with color effect
+const dynamicText = document.querySelector('.dynamic-text');
+const roles = [
+    "Frontend Developer", 
+    "Coder", 
+    "React.js Developer", 
+    "UI/UX Designer"
+];
+const colors = [
+    "#7d2ae8", // Original color
+    "#6c1ac2", // Darker shade
+    "#8a3de1", // Lighter shade
+    "#9a4ce6"  // Another shade
+];
+let currentIndex = 0;
+
+function changeText() {
+    dynamicText.textContent = roles[currentIndex];
+    dynamicText.style.color = colors[currentIndex];
+    currentIndex = (currentIndex + 1) % roles.length;
+}
+
+// Change text and color every 4 seconds
+setInterval(changeText, 4000);
